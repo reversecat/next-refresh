24fd214772c6591fd4c2f2461f7689408c9ef43e
Daniel Sobkowicz <daniel.a.sobkowicz@gmail.com>
2025-04-16 15:57:46 -0400

    Finish social app tut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { getPosts } from "@/actions/post.action";
+import { getDbUserId } from "@/actions/user.action";
+import CreatePost from "@/components/CreatePost";
+import ModeToggle from "@/components/ModeToggle";
+import PostCard from "@/components/PostCard";
+import { Button } from "@/components/ui/button";
+import WhoToFollow from "@/components/WhoToFollow";
+import prisma from "@/lib/prisma";
 import {
 	SignedIn,
 	SignedOut,
@@ -5,19 +13,35 @@ import {
 	SignUpButton,
 	UserButton,
 } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-export default function Home() {
-	return (
-		<div>
-			<SignedOut>
-				<SignInButton />
-				<SignUpButton />
-			</SignedOut>
+export default async function Home() {
+	const user = await currentUser();
+	const posts = await getPosts();
+	const dbUserId = await getDbUserId();
 
-			<SignedIn>
-				<UserButton />
-			</SignedIn>
+	console.log({ posts });
+
+	return (
+		<div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+			<div className="lg:col-span-6">
+				{user ? <CreatePost /> : null}
+
+				<div className="space-y-6">
+					{posts.map((post) => (
+						<PostCard
+							key={post.id}
+							post={post}
+							dbUserId={dbUserId}
+						/>
+					))}
+				</div>
+			</div>
+
+			<div className="hidden lg:block lg:col-span-4 sticky top-20">
+				<WhoToFollow />
+			</div>
 		</div>
 	);
 }
---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+async function TasksPage() {
+	const response = await fetch("http://localhost:3000/api/tasks", {
+		cache: "no-store",
+	});
+	const tasks = await response.json();
+	console.log(tasks);
+	return <div>TasksPage</div>;
+}
+
+export default TasksPage;
